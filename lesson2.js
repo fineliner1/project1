79df2252f28546a8ac1d2d58476715c034d3530b
Ferrum <example@gmail.com>
2021-04-28 12:02:12 +0200

    firstCommit

---
+++ lesson2.js
@@ -0,0 +1,24 @@
+'use strict';
+//concatination
+console.log('arr' + '- object');
+console.log(4 + +'5');
+//increment dicrement
+let incr = 10,
+    decr = 10;
+//++incr;
+//--decr;
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2); // ==1
+console.log(2*8 == '16'); // true
+console.log(2*8 === '16'); // строгое равенство
+
+// && == and
+// || == or
+//  ! == not
+const isChecked = true,
+      isClose = false;
+console.log(isChecked && isClose);
+console.log(isChecked || !isClose);
+console.log(1+1*5 !=7)
---
+++ script1.js
@@ -0,0 +1,15 @@
+'use strict';
+//alert('Hello');
+//const result = confirm('Are you here?');
+//console.log (result);
+//const answer = +prompt ('Do you have 20', '20');
+//console.log (answer + 10);
+const answers = [];
+answers[0] = prompt ('What is your name?', '');
+answers[1] = prompt ('What is your family?', '');
+answers[2] = prompt ('How old are you? ', '');
+//document.write (answers);
+console.log(typeof(answers));
+console.log (answers[2]);
+const name1 = answers[1];
+alert (`Hello, ${name1}`);
